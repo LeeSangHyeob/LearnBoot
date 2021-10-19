66b486d90cc648718bbfe41bd14ec89584f0527b
LeeSangHyeob <zerosmoon@naver.com>
2021-10-19 19:27:39 +0900

    3장 JPA

--- src/main/java/com/learn/book/springboot/Application.java
+++ src/main/java/com/learn/book/springboot/Application.java
@@ -2,8 +2,9 @@ package com.learn.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // 스프링 부트의 자동설정, Bean 읽기와 생성을 모두 자동으로 설정하게 되는 최상단 메인 클래스 설정
 public class Application {
     public static void main(String[] args){
---
+++ src/main/java/com/learn/book/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.learn.book.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // BaseTimeEntity 상속할 경우 BaseTimeEntity의 필드들도 컬럼으로 인식함
+@EntityListeners(AuditingEntityListener.class) // Auditing 기능을 포함
+public abstract class BaseTimeEntity {
+
+    @CreatedDate // Entity가 생성되어 저장될 때 시간이 자동 저장
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate // 조회한 Entity의 값을 변경할 때 시간이 자동 저장
+    private LocalDateTime modifiDate;
+}
