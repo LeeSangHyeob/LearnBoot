c4d90e96492256bbd9ac2bf5ca73a6d089034da9
LeeSangHyeob <zerosmoon@naver.com>
2021-10-28 16:38:40 +0900

    modifiDate -> modifiedDate 오탈자 수정

--- src/main/java/com/learn/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/learn/book/springboot/domain/BaseTimeEntity.java
@@ -18,5 +18,5 @@ public abstract class BaseTimeEntity {
     private LocalDateTime createdDate;
 
     @LastModifiedDate // 조회한 Entity의 값을 변경할 때 시간이 자동 저장
-    private LocalDateTime modifiDate;
+    private LocalDateTime modifiedDate;
 }
