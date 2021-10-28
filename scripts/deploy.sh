ebeabac276bde78c3c9ba6e554246cd4f0db35dc
LeeSangHyeob <zerosmoon@naver.com>
2021-10-28 13:57:52 +0900

    9장 Travis CI 설정 (CodeDeploy 설정)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/build/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 pid 확인"
-CURRENT_PID=$(pgrep -fl LearnBoot | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl LearnBoot | grep java | awk '{print $1}')
 echo "현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
@@ -27,6 +27,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-        -Dsrping.config/location=classpath:/application.properties,/home/ec2-user/app/application-oauth.peroperties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
-        -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
