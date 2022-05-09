e7df919780f81a974cd5e1b8e9aaea4a1abd06de
LeeSangHyeob <zerosmoon@naver.com>
2022-05-09 09:36:44 +0900

    네이버 맵 테스트 소스 코드 배포

--- out/production/resources/static/js/app/index.js
+++ out/production/resources/static/js/app/index.js
@@ -1,7 +1,7 @@
 var main = {
     init : function(){
         var _this = this;
-        $('#btn-save').on('click', function(){
+        /*$('#btn-save').on('click', function(){
             _this.save();
         });
 
@@ -11,10 +11,52 @@ var main = {
 
         $('#btn-delete').on('click', function(){
             _this.delete();
+        });*/
+
+        var map = new naver.maps.Map('naverMapArea');
+        var curtBtn = '<div style="display: inline-flex; align-items: center; position:relative; padding: 10px; background-color:#fff;"><img src="https://tistory4.daumcdn.net/tistory/3031640/skin/images/keypoint_red.svg"></div>';
+        var lat = '';
+        var lng = '';
+
+
+        var path = "testt".split('*')[0];
+        console.log(path);
+        var marker = new naver.maps.Marker({
+            position : new naver.maps.LatLng(37.5666805, 126.9784147),
+            map : map,
+            icon: {
+                //content: '<div><img src="https://tistory4.daumcdn.net/tistory/3031640/skin/images/keypoint_red.svg"><span>'
+                //        + "testtesttesttesttesttesttesttest" + '</span></div>'
+                content: '<div style="display: inline-flex; align-items: center; position:fixed; padding: 5px; border: 2px solid #4f7cef; border-radius: 30px; background-color:#fff;">'
+                              +'<span style="display: inline-block; width: 28px; hieght: 28px"><img src="https://tistory4.daumcdn.net/tistory/3031640/skin/images/keypoint_red.svg"></span>'
+                              +'<span style="display: inline-block; margin: 0 5px; font-size: 12px; font-weight: 700; line-height: 18px; vertical-align: middle">줌할때 마커 깨짐'
+                              +'<span style="display: block; position: absolute; left: 20px; bottom: -6px; width: 7px; height: 7px; border-bottom: 2px solid #4f7cef; border-right: 2px solid #4f7cef; transform: rotate(45deg); background-color:#fff;"></span></div>'
+            }
         });
 
+        marker.setMap(map);
+
+        naver.maps.Event.once(map, 'init', function(){
+            var csmCtrl = new naver.maps.CustomControl(curtBtn, {
+                position: naver.maps.Position.LEFT_TOP
+            });
+            csmCtrl.setMap(map);
+
+            naver.maps.Event.addDOMListener(csmCtrl.getElement(), 'click', function(){
+                console.log("Test");
+                if(lat == '' || lng == '') {
+                    console.log("latlng");
+                    lat = '37.5666805';
+                    lng = '126.9784147';
+                    test(map, lat, lng);
+                } else {
+                    map.setCenter(new naver.maps.LatLng(lat, lng));
+                }
+            });
+        });
 
     },
+
     save : function(){
         var data = {
             title: $('#title').val(),
@@ -70,7 +112,27 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+
+    encode : function(){
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/map/'+key,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (){
+            alert('인코딩');
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
+}
+
+test = function(map, val1, val2) {
+        lat = val1;
+        lng = val2;
+        map.setCenter(new naver.maps.LatLng(lat, lng));
 };
 
 main.init();
